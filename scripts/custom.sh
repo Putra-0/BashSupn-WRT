6d7768bcd934c4b6f17fd1677faf74b122058f9a
Adi Putra <73918462+Putra-0@users.noreply.github.com>
2024-02-03 11:47:25 +0700

    Update custom.sh

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -55,11 +55,11 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 
 # Add luci-app-tinyfilemanager
-#svn co https://github.com/lynxnexy/luci-app-tinyfilemanager/trunk package/luci-app-tinyfilemanager
-svn co https://github.com/helmiau/helmiwrt-packages/trunk/luci-app-tinyfm package/luci-app-tinyfm
+svn co https://github.com/lynxnexy/luci-app-tinyfilemanager/trunk package/luci-app-tinyfilemanager
+#svn co https://github.com/helmiau/helmiwrt-packages/trunk/luci-app-tinyfm package/luci-app-tinyfm
 
-svn co https://github.com/helmiau/helmiwrt-packages/trunk/badvpn package/badvpn
-svn co https://github.com/helmiau/helmiwrt-packages/trunk/corkscrew package/corkscrew
+#svn co https://github.com/helmiau/helmiwrt-packages/trunk/badvpn package/badvpn
+#svn co https://github.com/helmiau/helmiwrt-packages/trunk/corkscrew package/corkscrew
 
 # Add luci-app-openclash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
